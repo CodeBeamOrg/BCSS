7ea96defb72cc9e0bc22881dea1b1de1b9f16dc8
mckaragoz <78308169+mckaragoz@users.noreply.github.com>
2023-08-20 16:28:52 +0300

    Add Docs

---
+++ BCSSViewer.Wasm/Program.cs
@@ -0,0 +1,17 @@
+using BCSS;
+using BCSSViewer.Wasm;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using MudExtensions.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddMudExtensions();
+builder.Services.AddScoped<BCSSService>();
+
+await builder.Build().RunAsync();
